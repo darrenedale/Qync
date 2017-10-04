e1fa04f1ebc9106a4cb33f9eef4085ba0648b535
Darren Edale <darren@equituk.net>
2017-10-04 22:20:16 +0100

    Finished documentation of NotificationWidget class.

--- src/notificationwidget.cpp
+++ src/notificationwidget.cpp
@@ -11,8 +11,6 @@
  * - notificationwidget.ui
  * - QDebug
  * - QPropertyAnimation
- *
- * \todo Make warning/error colours configurable?
  */
 
 #include "notificationwidget.h"
@@ -25,6 +23,42 @@
 namespace Qync {
 
 
+	/**
+	 * \class NotificationWidget
+	 * \author Darren Edale
+	 * \date October 2017
+	 * \version 1.1.0
+	 *
+	 * \brief Widget to show unobtrusive notifications.
+	 *
+	 * This widget provides a pop-up widget that animates gracefully into view when it is
+	 * shown and animates gracefully out again when dismissed. This provides the user with
+	 * an experience that is not as jarring as popping up a dialogue window with a message
+	 * immediately in front of the user. It does not get in the way of the existing UI or
+	 * steal focus from it, and does not suddenly change the visuals in front of the user.
+	 *
+	 * It supports three types of notification: information, warning and error. Information
+	 * messages are unobtrusive, just appearing like a normal label text; warning messages
+	 * appear with a yellow background; error messages with a red background. The colours
+	 * are relatively subtle, just enough to draw attention without jarring.
+	 *
+	 * The intended us for this widget is as a usually-hidden widget embedded in a layout,
+	 * which is made visible when it has a message to show. It is suitable for relatively
+	 * simple, short messages that the user can dismiss. They do not support any other user
+	 * interaction, such as questions or alternative responses. The messages can be used
+	 * with Qt's HTML-like markup for rich content.
+	 *
+	 * To use the widget call setMessage() then call show(). If the widget is already
+	 * visible, it will be updated to show the provided content. If it is not visible, it
+	 * will have the content set and will be gently scrolled into view.
+	 */
+
+
+	/**
+	 * \brief Create a NotificationWidget
+	 *
+	 * \param parent The owning parent for the created widget.
+	 */
 	NotificationWidget::NotificationWidget(QWidget * parent)
 	: QWidget(parent),
 	  m_ui(new Ui::NotificationWidget),
@@ -47,19 +81,41 @@ namespace Qync {
 	}
 
 
+	/** \brief Destroy the widget. */
 	NotificationWidget::~NotificationWidget(void) = default;
 
 
+	/**
+	 * \brief Fetch the message currently shown in the widget.
+	 *
+	 * \return The message.
+	 */
 	QString NotificationWidget::message() const {
 		return m_ui->message->text();
 	}
 
 
+	/**
+	 * \brief Set the message currently shown in the widget.
+	 *
+	 * \param message The message.
+	 *
+	 * Setting the message does not automatically render the widget visible. To do this,
+	 * call show().
+	 */
 	void NotificationWidget::setMessage(const QString & message) {
 		m_ui->message->setText(message);
 	}
 
 
+	/**
+	 * \brief Set the type of notifivation.
+	 *
+	 * \param type The type.
+	 *
+	 * The type will be set and the visuals of the widget will immediately be updated
+	 * appropriately for the new type.
+	 */
 	void NotificationWidget::setType(NotificationType type) {
 		if(type != m_type) {
 			m_type = type;
@@ -82,6 +138,19 @@ namespace Qync {
 	}
 
 
+	/**
+	 * \brief Make the notification widget visible.
+	 *
+	 * If the widget is not currently visible, it will be gently scrolled into
+	 * view. If it is already visible, or if the animation to scroll a previous
+	 * message into view is in progress (i.e. show() has been called recently
+	 * and its work has not yet finished), no action will be taken.
+	 *
+	 * This method is reimplemented from its base class but it is not virtual.
+	 * This means that if the widget object is referenced by a base-class
+	 * pointer or reference, the show() method as defined in that base class
+	 * will be called, not this reimplementation.
+	 */
 	void NotificationWidget::show(void) {
 		if(m_showAnim->state() != QPropertyAnimation::Stopped) {
 			qWarning() << "show animation is already running";
@@ -101,6 +170,19 @@ namespace Qync {
 	}
 
 
+	/**
+	 * \brief Hide the notification widget.
+	 *
+	 * If the widget is currently visible, it will be gently scrolled out of
+	 * view. If it is already hidden, or if the animation to scroll it out of
+	 * view is in progress (i.e. hide() has been called recently and its work
+	 * has not yet finished), no action will be taken.
+	 *
+	 * This method is reimplemented from its base class but it is not virtual.
+	 * This means that if the widget object is referenced by a base-class
+	 * pointer or reference, the hide() method as defined in that base class
+	 * will be called, not this reimplementation.
+	 */
 	void NotificationWidget::hide(void) {
 		if(m_hideAnim->state() != QPropertyAnimation::Stopped) {
 			qWarning() << "hide animation is already running";
